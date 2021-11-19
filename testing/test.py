0edce3b9984c606053befbabf6ac24e89997d854
Nisha Gupta <nisgup97@in.ibm.com>
2021-11-19 23:18:05 +0530

    Testing github actions
    Signed-off-by: Nisha Gupta <nisgup97@in.ibm.com>

--- testing/test.py
+++ testing/test.py
@@ -3,23 +3,36 @@ import requests
 
 import json
 
-apiurl = 'https://localhost:8000'
-def test_post_headers_body_json():
-    url = apiurl + '/mark'
-    
-    # Additional headers.
-    headers = {'Content-Type': 'application/json' } 
+apiurl = 'http://localhost:8000'
 
-    # Body
-    payload = {'user_id': "test", 'flagged_string': "test string", 'category': "stereotyping", 'info': "none", 'url': "example.com"}
-    
-    # convert dict to json by json.dumps() for body data. 
-    resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))       
-    
+
+def test_get_read_categories():
+    url = apiurl + '/categories'
+    resp = requests.get(url)
+
     # Validate response headers and body contents, e.g. status code.
     assert resp.status_code == 200
-    resp_body = resp.json()
-    assert resp_body['url'] == url
-    
+
     # print response full body as text
     print(resp.text)
+
+
+# def test_post_headers_body_json():
+#     url = apiurl + '/mark'
+#
+#     # Additional headers.
+#     headers = {'Content-Type': 'application/json' }
+#
+#     # Body
+#     payload = {'user_id': "test", 'flagged_string': "test string", 'category': "stereotyping", 'info': "none", 'url': "example.com"}
+#
+#     # convert dict to json by json.dumps() for body data.
+#     resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))
+#
+#     # Validate response headers and body contents, e.g. status code.
+#     assert resp.status_code == 200
+#     resp_body = resp.json()
+#     assert resp_body['url'] == url
+#
+#     # print response full body as text
+#     print(resp.text)
