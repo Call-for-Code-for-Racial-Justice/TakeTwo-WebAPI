a3052e966f3649c417e04a0e14f257accb67b19a
Holdrick <taylorjcooper28@gmail.com>
2022-03-01 09:42:18 -0700

    Issue38 - Added a couple more cases for analyse_text
    Signed-off-by: Holdrick <taylorjcooper28@gmail.com>

--- testing/unit/test_analyse_text.py
+++ testing/unit/test_analyse_text.py
@@ -12,7 +12,35 @@ from couchdb.client import Document
 from couchdb.client import Row
 
 class TestTakeTwoWebApi(unittest.TestCase):
-    def test_happy_path(self):
+
+    def test_multiple_items_with_flagged_string(self):
+        firstDoc = Document()
+        firstDoc["flagged_string"] = "bad_string"
+        firstDoc["category"] = "the_category"
+        firstDoc["info"] = "the_info"
+        firstRow = Row()
+        firstRow['doc'] = firstDoc
+
+        secondDoc = Document()
+        secondDoc["flagged_string"] = "bad_string_2"
+        secondDoc["category"] = "the_category"
+        secondDoc["info"] = "the_info"
+        secondRow = Row()
+        secondRow['doc'] = secondDoc
+        mockDbViewResults = MagicMock()
+        mockDbViewResults.__iter__ = MagicMock(return_value=iter([firstRow, secondRow]))
+        db.view = MagicMock(return_value=mockDbViewResults)
+
+        data = {'content': 'bad_string, bad_string_2'}
+        text = Text(**data)
+        output = analyse_text(text)
+
+        expected = {"biased": 
+                        [{"flag": "bad_string", "category": "the_category", "info": "the_info"},
+                         {"flag": "bad_string_2", "category": "the_category", "info": "the_info"}]}
+        assert output == expected, "Actual: " + str(output) + " Expected: " + str(expected)
+
+    def test_single_item_with_flagged_string(self):
         firstDoc = Document()
         firstDoc["flagged_string"] = "bad_string"
         firstDoc["category"] = "the_category"
@@ -31,7 +59,18 @@ class TestTakeTwoWebApi(unittest.TestCase):
                         [{"flag": "bad_string", "category": "the_category", "info": "the_info"}]}
         assert output == expected, "Actual: " + str(output) + " Expected: " + str(expected)
 
-        print(output)
+    def test_no_items_in_db(self):
+        mockDbViewResults = MagicMock()
+        mockDbViewResults.__iter__ = MagicMock(return_value=iter([]))
+        db.view = MagicMock(return_value=mockDbViewResults)
+
+        data = {'content': 'bad_string'}
+        text = Text(**data)
+        output = analyse_text(text)
+
+        expected = {"biased": 
+                        []}
+        assert output == expected, "Actual: " + str(output) + " Expected: " + str(expected)
 
 if __name__ == '__main__':
     unittest.main()
